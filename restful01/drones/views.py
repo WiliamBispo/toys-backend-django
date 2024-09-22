cbad828ed7e2a50b55d2fc23fc0f551a877f0a41
Wiliam Bispo <wiliam_bis14@hotmail.com>
2024-09-22 13:10:35 -0300

    update views and create urls

--- restful01/drones/views.py
+++ restful01/drones/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from drones.models import DroneCategory
+from drones.models import Drone
+from drones.models import Pilot
+from drones.models import Competition
+from drones.serializers import DroneCategorySerializer
+from drones.serializers import DroneSerializer
+from drones.serializers import PilotSerializer
+from drones.serializers import PilotCompetitionSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ApiRoot(generics.GenericAPIView):
+    name = "api-root"
+
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "drone-categories": reverse("dronecategory-list", request=request),
+                "drones": reverse(DroneList.name, request=request),
+                "pilots": reverse(PilotList.name, request=request),
+                "competitions": reverse(CompetitionList.name, request=request),
+            }
+        )
+
+
+class DroneCategoryViewSet(viewsets.ModelViewSet):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+
+
+class DroneList(generics.ListCreateAPIView):
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = "drone-list"
+
+
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = "drone-detail"
+
+
+class PilotList(generics.ListCreateAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = "pilot-list"
+
+
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = "pilot-detail"
+
+
+class CompetitionList(generics.ListCreateAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = "competition-list"
+
+
+class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = "competition-detail"
