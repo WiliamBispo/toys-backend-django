3f9058e52b009cea4ec1977b990c227e1763ae77
Wiliam Bispo <wiliam_bis14@hotmail.com>
2024-10-03 09:11:56 -0300

    Inserção de Filtros

--- restful01/drones/views.py
+++ restful01/drones/views.py
@@ -9,6 +9,7 @@ from drones.serializers import DroneCategorySerializer
 from drones.serializers import DroneSerializer
 from drones.serializers import PilotSerializer
 from drones.serializers import PilotCompetitionSerializer
+from drones.filters import CompetitionFilter
 from rest_framework import viewsets
 
 
@@ -29,12 +30,17 @@ class ApiRoot(generics.GenericAPIView):
 class DroneCategoryViewSet(viewsets.ModelViewSet):
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
+    search_fields = ("^name",)
+    ordering_fields = ("name",)
 
 
 class DroneList(generics.ListCreateAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
     name = "drone-list"
+    filterset_fields = ("drone_category", "has_it_competed",)
+    search_fields = ("^name",)
+    ordering_fields = ("name", "manufacturing_date",)
 
 
 class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -47,6 +53,9 @@ class PilotList(generics.ListCreateAPIView):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = "pilot-list"
+    filterset_fields = ("gender", "races_count",)
+    search_fields = ("^name",)
+    ordering_fields = ("name", "races_count")
 
 
 class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -59,6 +68,11 @@ class CompetitionList(generics.ListCreateAPIView):
     queryset = Competition.objects.all()
     serializer_class = PilotCompetitionSerializer
     name = "competition-list"
+    filterset_class = CompetitionFilter
+    ordering_fields = (
+        "distance_in_feet",
+        "distance_achievement_date",
+    )
 
 
 class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'toys.apps.ToysConfig',
     # habilita o Drones application
     'drones.apps.DronesConfig',
+    # Django Filters
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -131,4 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "drones.pagination.LimitOffsetPaginationWithUpperBound",
     "PAGE_SIZE": 4,
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ),
 }
