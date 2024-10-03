79c3a67ca82db5efa8475fa3d439d8cff70bc38c
Wiliam Bispo <wiliam_bis14@hotmail.com>
2024-10-03 08:36:20 -0300

    Inserção de Constrainsts e Paginação

--- restful01/drones/models.py
+++ restful01/drones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DroneCategory(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -12,7 +12,7 @@ class DroneCategory(models.Model):
 
 
 class Drone(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     drone_category = models.ForeignKey(
         DroneCategory, related_name='drones', on_delete=models.CASCADE)
     manufacturing_date = models.DateField()
@@ -30,7 +30,8 @@ class Pilot(models.Model):
     MALE = 'M'
     FEMALE = 'F'
     GENDER_CHOICES = ((MALE, 'Male'), (FEMALE, 'Female'),)
-    name = models.CharField(max_length=150, blank=False, default='')
+    name = models.CharField(max_length=150, blank=False,
+                            default='', unique=True)
     gender = models.CharField(
         max_length=2, choices=GENDER_CHOICES, default=MALE,)
     races_count = models.IntegerField()
